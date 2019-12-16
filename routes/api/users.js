84a286f2db6e4aae935edba099d53ec45a9359ff
tzetzo <tzvetanmarinov@yahoo.com>
2019-12-16 16:15:31 +0200

    1. connect to MongoDB with mongoose 2. setup route files with express.Router() 3. User model created 4. req.body validation with "express-validator" 5. Setup Registering user route & returning JWT

---
+++ routes/api/users.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+// @router    POST api/users
+// @desc      Register user
+// @access    Public
+router.post(
+  "/",
+  [
+    //https://express-validator.github.io/docs/index.html
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get user gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({ name, email, avatar, password });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Register/save user in MongoDB
+      await user.save();
+
+      // Return jsonwebtoken;
+      // https://github.com/auth0/node-jsonwebtoken;
+      // https://jwt.io/
+
+      jwt.sign(
+        {
+          user: {
+            id: user.id
+          }
+        },
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600 * 30 * 24 * 60 * 60
+        },
+        (e, token) => {
+          if (e) throw e;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware - to be able to use req.body;
+// bodyParser was a separate package but now is included in express
+// so no more app.use(bodyParser.json()) but instead:
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running for Tzetzo"));
+
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
