8cd7e81dbb09d1236d621b3b845d5ce8530d3d67
tzetzo <tzvetanmarinov@yahoo.com>
2019-12-17 07:13:54 +0200

    Setup Login user route & returning JWT

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DEVCONNECTOR
+
+# POST api/users Public -> Register/Sign up user
+
+# POST api/auth Public -> Login/Sign in user
+
+# GET api/auth Private -> Get current user
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
 // @router    GET api/auth
@@ -15,5 +19,66 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @router    POST api/auth
+// @desc      Login/Sign in user
+// @access    Public
+router.post(
+  "/",
+  [
+    //https://express-validator.github.io/docs/index.html
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // check if supplied password matches the one in the DB
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Return jsonwebtoken;
+      // https://github.com/auth0/node-jsonwebtoken;
+      // https://jwt.io/
+      jwt.sign(
+        {
+          user: {
+            id: user.id
+          }
+        },
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600 * 30 * 24 * 60 * 60
+        },
+        (e, token) => {
+          if (e) throw e;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
