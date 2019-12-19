cb4787634f836624cac2061e5d4cb87765773494
tzetzo <tzvetanmarinov@yahoo.com>
2019-12-19 16:36:05 +0200

    1. script to run concurrently express server & webpack dev-server 2. create-react-app install 3. font-awesome install

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Express server started on port ${PORT}`));
