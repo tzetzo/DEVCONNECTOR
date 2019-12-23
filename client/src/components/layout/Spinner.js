2b8e6c1416b7d5540af83ba8cfde161ca0cb4e81
tzetzo <tzvetanmarinov@yahoo.com>
2019-12-23 19:49:20 +0200

    1. spinner version with .gif 2. spinner version with CSS

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Spinner.css";
+
+export default () => <div className="loader">Loading...</div>;
+
+// import React from "react";
+// import spinner from "../../img/spinner.gif";
+//
+// export default () => (
+//   <React.Fragment>
+//     <img
+//       style={{ width: "200px", margin: "auto", display: "block" }}
+//       alt={"Loading..."}
+//       src={spinner}
+//     />
+//   </React.Fragment>
+// );
