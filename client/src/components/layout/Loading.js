9177a1353a6288cfd93ed31a7e8b521e2686f314
tzetzo <tzvetanmarinov@yahoo.com>
2019-12-29 12:25:50 +0200

    bug fix

--- client/src/components/layout/Loading.js
+++ client/src/components/layout/Loading.js
@@ -2,14 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import Spinner from "./Spinner";
 
-const Loading = ({ loading, loadingProfile, children }) => {
-  if (loading || loadingProfile) return <Spinner />;
+const Loading = ({ loading, children }) => {
+  if (loading) return <Spinner />;
 
   return children;
 };
 
 const mapStateToProps = ({ auth, profile }) => {
-  return { loading: auth.loading, loadingProfile: profile.loading };
+  return { loading: auth.loading };
 };
 
 export default connect(mapStateToProps)(Loading);
