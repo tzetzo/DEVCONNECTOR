ec8c4446c0936fedbc103fe1efd2cd4f1503fe79
tzetzo <tzvetanmarinov@yahoo.com>
2020-01-06 08:39:49 +0200

    1. heroku-postbuild script 2. server.js to serve index.html in production

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ connectDB();
 // so no more app.use(bodyParser.json()) but instead:
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running for Tzetzo"));
-
 // Define routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/users", require("./routes/api/users"));
+
+// Serve our React app in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
